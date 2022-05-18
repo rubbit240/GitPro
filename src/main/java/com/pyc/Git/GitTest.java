e0aec58f9505e618e12d14353d1313e051891ffc
Pan <Pan@atguigu.com>
2022-05-19 00:51:03 +0800

    push commit

--- src/main/java/com/pyc/Git/GitTest.java
+++ src/main/java/com/pyc/Git/GitTest.java
@@ -5,5 +5,6 @@ public class GitTest {
         System.out.println("hello,git1");
         System.out.println("hot-fix commit");
         System.out.println("master commit");
+        System.out.println("push test");
     }
 }
